1a40c926809a20cee01c2c80c3cc2d1035cd984c
Kenji <kenji.arakaki93@gmail.com>
2020-11-20 12:07:56 -0300

    Sensores con js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let square = document.getElementById("square");
+    
+    window.addEventListener('deviceorientation', function(e) {
+        a = Math.floor(e.alpha);
+        b = Math.floor(e.beta);
+        g = Math.floor(e.gamma);
+        let transform = 'rotateZ(' + Math.round(e.alpha) + 'deg) rotateX(' + Math.round(e.beta -90) + 'deg) rotateY(' + Math.round(e.gamma) + 'deg)';
+        square.style.transform = transform;   
+    });
+})
