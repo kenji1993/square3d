e75de8859eae1bc82d029a6286cf86db6e74dc26
Kenji <kenji.arakaki93@gmail.com>
2020-11-20 12:22:16 -0300

    cambio de deg

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
         a = Math.floor(e.alpha);
         b = Math.floor(e.beta);
         g = Math.floor(e.gamma);
-        let transform = 'rotateZ(' + Math.round(e.alpha) + 'deg) rotateX(' + Math.round(e.beta -90) + 'deg) rotateY(' + Math.round(e.gamma) + 'deg)';
+        let transform = 'rotateZ(' + Math.round(e.alpha) + 'deg) rotateX(' + Math.round(e.beta -60) + 'deg) rotateY(' + Math.round(e.gamma) + 'deg)';
         square.style.transform = transform;   
     });
 })
